47d5fdc03727f049f4c633636484410627ed92f2
Kavaldrin <turbo3001997@gmail.com>
2018-12-01 12:30:37 +0100

    Create main.cpp
    hihi

---
+++ 1/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <numeric>
+#include <thread>
+
+using std::fstream;
+using std::cout; using std::endl;
+
+void taskA(const std::vector<int>& vec,int& solution)
+{
+	 solution = std::accumulate(vec.cbegin(), vec.cend(), 0);
+}
+void taskB(const std::vector<int>& vec, int& solution)
+{
+	std::vector<int> frequences; frequences.push_back(0);
+	while (true) {
+		for (auto& f : vec) {
+			int newfreq = frequences.back() + f;
+			for (auto &existingf : frequences) {
+				if (newfreq == existingf) {
+					solution = newfreq;
+					return;
+				}
+			}
+			frequences.push_back(newfreq);
+		}
+	}
+}
+
+
+int main()
+{
+	fstream file("input.txt", std::ios::in);
+	if (!file.good()) std::runtime_error("Cannot open the file\n");
+	std::vector<int> data;
+	while (!file.eof()) {
+		int temp;
+		file >> temp;
+		data.push_back(temp);
+	}
+
+	int solutionA;
+	int solutionB;
+
+	std::thread t1(taskA, std::ref(data),std::ref(solutionA));
+	std::thread t2(taskB, std::ref(data),std::ref(solutionB));
+	t1.join(); t2.join();
+	
+	cout << "A: " << solutionA << " B: " << solutionB << endl;
+
+	system("pause");
+
+}
